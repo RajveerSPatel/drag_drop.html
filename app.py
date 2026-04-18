8c1de8e43147d369b978a8b84fda42c4915a4073
RajveerSPatel <raghav.kerai55@gmail.com>
2026-04-18 01:58:36 +0000

    app.py add directions

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route("/")
+#Home to Calendar
+@app.route('/')
+def home():
+    return render_template('calendar.html')
 
+#Calendar page
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
 
+#Day view page
+@app.route('/day-view')
+def day_view():
+    return render_template('day-view.html')
 
-if __name__ == "__main__":
+#Lesson creator page
+@app.route('/lesson-creator')
+def lesson_creator():
+    return render_template('lesson-creator.html')
+
+#Lesson library page
+@app.route('/lesson-library')
+def lesson_library():
+    return render_template('lesson-library.html')
+
+#Serve JS files
+@app.route('/js/<path:filename>')
+def serve_js(filename):
+    return send_from_directory('js', filename)
+
+#Serve CSS files
+@app.route('/css/<path:filename>')
+def serve_css(filename):
+    return send_from_directory('css', filename)
+
+if __name__ == '__main__':
     app.run(debug=True)
